ad495b1544344cb68f82d9214d487ffe343beb7e
YoncaCoskun <yoncacoskun92@gmail.com>
2015-05-01 22:05:37 +0300

    InsertDeleteCommand

---
+++ LibraryRegisterInsertDeleteCommand/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace LibraryRegisterInsertDeleteCommand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtWriter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHouse = new System.Windows.Forms.TextBox();
+            this.txtPages = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtlibraryId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(48, 151);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(460, 177);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Library ID";
+            this.columnHeader1.Width = 59;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Book Name";
+            this.columnHeader2.Width = 118;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Writer";
+            this.columnHeader3.Width = 82;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Publishing House";
+            this.columnHeader4.Width = 96;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Number of Page";
+            this.columnHeader5.Width = 95;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(134, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Firebrick;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(258, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(152, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Book Name :";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(258, 41);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(129, 20);
+            this.txtBookName.TabIndex = 4;
+            // 
+            // txtWriter
+            // 
+            this.txtWriter.Location = new System.Drawing.Point(258, 68);
+            this.txtWriter.Name = "txtWriter";
+            this.txtWriter.Size = new System.Drawing.Size(129, 20);
+            this.txtWriter.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(190, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Writer :";
+            // 
+            // txtHouse
+            // 
+            this.txtHouse.Location = new System.Drawing.Point(258, 94);
+            this.txtHouse.Name = "txtHouse";
+            this.txtHouse.Size = new System.Drawing.Size(129, 20);
+            this.txtHouse.TabIndex = 7;
+            // 
+            // txtPages
+            // 
+            this.txtPages.Location = new System.Drawing.Point(258, 120);
+            this.txtPages.Name = "txtPages";
+            this.txtPages.Size = new System.Drawing.Size(129, 20);
+            this.txtPages.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(108, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Publishing House :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(108, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Number Of Pages :";
+            // 
+            // txtlibraryId
+            // 
+            this.txtlibraryId.Location = new System.Drawing.Point(258, 15);
+            this.txtlibraryId.Name = "txtlibraryId";
+            this.txtlibraryId.Size = new System.Drawing.Size(129, 20);
+            this.txtlibraryId.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(152, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Library ID :";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Firebrick;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(374, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 43);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(554, 389);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtlibraryId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPages);
+            this.Controls.Add(this.txtHouse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWriter);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Library Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtWriter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHouse;
+        private System.Windows.Forms.TextBox txtPages;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtlibraryId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LibraryRegisterInsertDeleteCommand/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryRegisterInsertDeleteCommand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connect = new SqlConnection("server=.;database=Library;integrated security=sspi;");
+
+        private void showData()
+        {
+            listView1.Items.Clear();
+
+            connect.Open();
+            SqlCommand command = new SqlCommand("select * from tblBooks", connect);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ListViewItem row = new ListViewItem(reader["libraryId"].ToString());
+                row.SubItems.Add(reader["bookname"].ToString());
+                row.SubItems.Add(reader["writer"].ToString());
+                row.SubItems.Add(reader["publishinghouse"].ToString());
+                row.SubItems.Add(reader["numberofpages"].ToString());
+
+                listView1.Items.Add(row);
+            }
+
+
+            connect.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showData();
+
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            connect.Open();
+            SqlCommand command = new SqlCommand("insert into tblBooks(libraryId,bookname,writer,publishinghouse,numberofpages) values('" + txtlibraryId.Text + "','" + txtBookName.Text + "','" + txtWriter.Text + "','" + txtHouse.Text + "','" + txtPages.Text + "')", connect);
+            command.ExecuteNonQuery();
+            connect.Close();
+
+            listView1.Items.Clear();
+
+            showData();
+
+
+
+            txtBookName.Clear();
+            txtHouse.Clear();
+            txtlibraryId.Clear();
+            txtPages.Clear();
+            txtWriter.Clear();
+
+
+        }
+
+        int id = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            connect.Open();
+            SqlCommand command = new SqlCommand("delete from tblBooks where libraryId=(" + id + ")", connect);
+            command.ExecuteNonQuery();
+            connect.Close();
+            showData();
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            txtlibraryId.Text = listView1.SelectedItems[0].SubItems[0].Text;
+
+            id = int.Parse(txtlibraryId.Text);
+
+
+            txtBookName.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            txtWriter.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            txtHouse.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            txtPages.Text = listView1.SelectedItems[0].SubItems[4].Text;
+
+
+
+        }
+
+
+    }
+}
